2a6fbab758465679810be53b6706de552850d685
Agathius <p.akos1148@gmail.com>
2023-07-28 10:36:23 +0200

    added cors allowed origin

--- src/config/allowedOrigins.js
+++ src/config/allowedOrigins.js
@@ -1,4 +1,4 @@
 // list of allowed origins
-const allowedOrigins = ['http://127.0.0.1:5173']; 
+const allowedOrigins = ['http://127.0.0.1:5173, https://geolens-gallery-api.onrender.com']; 
 
 module.exports = allowedOrigins;

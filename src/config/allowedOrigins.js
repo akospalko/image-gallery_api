7e0ac7ff606d37ba302bce8f99c0d32ec2926a54
Agathius <p.akos1148@gmail.com>
2023-07-28 10:59:15 +0200

    fixed allow origins 2

--- src/config/allowedOrigins.js
+++ src/config/allowedOrigins.js
@@ -1,4 +1,4 @@
 // list of allowed origins
-const allowedOrigins = ['http://127.0.0.1:5173, https://geolens-gallery.onrender.com']; 
+const allowedOrigins = ['http://127.0.0.1:5173', 'https://geolens-gallery.onrender.com']; 
 
 module.exports = allowedOrigins;
